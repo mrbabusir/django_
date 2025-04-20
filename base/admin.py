36a88a267a2dfa1a56c8a9cc546e0abb439a1fae
Pravin Magar <pravinrokamagar86@gmail.com>
2025-04-20 16:00:01 +0545

    Todolist admin panel username and password created and edited,.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Todolist
 # Register your models here.
+@admin.register(Todolist)
+class TodolistAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'is_completed')
+    list_filter = ('is_completed',)
+    search_fields = ('title', 'description')
+    list_per_page = 8
+    list_editable = ('title', 'is_completed')
+    # ordering = ('-id',)
+# admin.site.register(Todolist)
--- base/models.py
+++ base/models.py
@@ -5,3 +5,5 @@ class Todolist(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
+    def __str__ (self):
+        return (f"{self.title},{self.is_completed}")
